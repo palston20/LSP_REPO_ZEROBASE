eabd2c25db1fe84801192e0f3c06948e4dedc634
Paris Alston <parisalston0@gmail.com>
2025-01-23 16:29:37 -0500

    First commit.

---
+++ LSP_Homework1/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Paris Alston
+ */
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
