73d8136e8890db66732fa2b9f355ee072511af47
Paris Alston <parisalston0@gmail.com>
2025-03-20 17:46:26 -0400

    trying javadocs

--- LSP_Homework1/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
+++ LSP_Homework1/src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -1,12 +1,33 @@
 package org.howard.edu.lsp.midterm.question1;
 
+/**
+ * Program to Add integers 
+ * @author Paris Alston
+ */
+
 public class HelloWorld {
+	/**
+     * Adds two integers.
+     * 
+     * @param a The first integer
+     * @param b The second integer
+     * @return The sum of a and b
+     */
+
+	
+	public static int add(int a, int b) {
+		return a + b;
+		}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		 // Call the add method
+		 int result = add(3, 4);
 
+		 // Print the result and "Hello World!"
+		 System.out.println("Hello World!");
+		 System.out.println("The sum of 3 and 4 is: " + result);
+		    }
 
 	}
 
-}
+
